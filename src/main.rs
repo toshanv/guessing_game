51059adf909016fa98be7def4a484c734427f6ce
Toshan Vysyaraju <toshanv2000@gmail.com>
2021-05-19 13:25:53 -0400

    [ADD] read user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// io library from the standard library
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // 'let' is used to create a variable
+        // variables are immutable by default
+    // 'mut' makes the variable mutable
+    let mut guess = String::new();
+
+    // get input from user
+    io::stdin()
+        // 'read_line' appends input to the parameter
+        // '&' indicates that the argument is a reference (immutable by default)
+        // 'mut' makes the reference mutable
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // print user input
+    println!("You guessed: {}", guess);
 }
