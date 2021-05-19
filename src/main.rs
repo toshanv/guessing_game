22cc95fe9daa93c8cd7d192d8a37ba10d6f5678a
Toshan Vysyaraju <toshanv2000@gmail.com>
2021-05-19 15:04:46 -0400

    [ADD] multiple guesses

--- src/main.rs
+++ src/main.rs
@@ -10,36 +10,41 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // 'let' is used to create a variable
-    // variables are immutable by default
-    // 'mut' makes the variable mutable
-    let mut guess = String::new();
+        // 'let' is used to create a variable
+        // variables are immutable by default
+        // 'mut' makes the variable mutable
+        let mut guess = String::new();
 
-    // get input from user
-    io::stdin()
-        // 'read_line' appends input to the parameter
-        // '&' indicates that the argument is a reference (immutable by default)
-        // 'mut' makes the reference mutable
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    // shadow the int guess onto the existing string guess
-    // u32: unsigned, 32-bit integer (good default for small, positive numbers)
-    let guess: u32 = guess
-        // removes white space from beginning and end
-        .trim()
-        // parses string into a u32 (specified above)
-        .parse()
-        .expect("Please type a number!");
+        // get input from user
+        io::stdin()
+            // 'read_line' appends input to the parameter
+            // '&' indicates that the argument is a reference (immutable by default)
+            // 'mut' makes the reference mutable
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // print user input
-    println!("You guessed: {}", guess);
+        // shadow the int guess onto the existing string guess
+        // u32: unsigned, 32-bit integer (good default for small, positive numbers)
+        let guess: u32 = guess
+            // removes white space from beginning and end
+            .trim()
+            // parses string into a u32 (specified above)
+            .parse()
+            .expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // print user input
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
